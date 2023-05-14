c183090682ce3129afc772826a002158f8a60c75
omar2432 <omar.ahmed2432@gmail.com>
2023-05-14 08:05:10 +0300

    FOrm error handling done

---
+++ lib/models/user.dart
@@ -0,0 +1,12 @@
+class User{
+  String? firstName;
+  String? lastName;
+  String? address;
+  String? area;
+  String? landline;
+  String? mobile;
+  String? idFront;
+  String? idBack;
+
+  User({this.firstName,this.lastName,this.address,this.area,this.landline,this.mobile,this.idFront,this.idBack});
+}
--- lib/screens/reg_screen.dart
+++ lib/screens/reg_screen.dart
@@ -1,9 +1,9 @@
 import 'package:edge_detection/edge_detection.dart';
 import 'package:flutter/material.dart';
 import 'dart:io';
-import 'package:image_picker/image_picker.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
+import '../models/user.dart';
 
 class RegScreen extends StatefulWidget {
   const RegScreen({super.key});
@@ -13,8 +13,21 @@ class RegScreen extends StatefulWidget {
 }
 
 class _RegScreenState extends State<RegScreen> {
+  var _editedUser = User(
+    firstName: '',
+    lastName: '',
+    address: '',
+    area: '',
+    landline: '',
+    mobile: '',
+    idFront: '',
+    idBack: '',
+  );
+
   final List<String> Areas = ['Cairo', 'Giza', 'Alexandria'];
   String? selectedArea;
+
+  final _form = GlobalKey<FormState>();
 
   File? id_front;
   File? id_back;
@@ -31,8 +44,7 @@ class _RegScreenState extends State<RegScreen> {
       androidCropReset: 'Reset',
     );
 
-
-    setState(() {      
+    setState(() {
       id_front = File(imagePath);
     });
   }
@@ -49,10 +61,26 @@ class _RegScreenState extends State<RegScreen> {
       androidCropReset: 'Reset',
     );
 
-
-    setState(() {      
+    setState(() {
       id_back = File(imagePath);
     });
+  }
+
+  void _saveForm() {
+    bool isvalid=_form.currentState!.validate();
+    if(id_front!=null&&id_back!=null){
+    if(isvalid){
+    _form.currentState!.save();
+    _editedUser.idFront=id_front!.path;
+    _editedUser.idBack=id_back!.path;   
+    }
+    }else{
+      ScaffoldMessenger.of(this.context).showSnackBar(const SnackBar(
+      content: Text("You must take a picture of both the front and the back of the ID"),
+    ));
+    }
+
+    
   }
 
   @override
@@ -60,111 +88,182 @@ class _RegScreenState extends State<RegScreen> {
     return Scaffold(
       appBar: AppBar(
         title: Text("BLNK Registration Form"),
+        actions: <Widget>[
+          IconButton(onPressed: _saveForm, icon: Icon(Icons.save)),
+        ],
       ),
       body: Padding(
         padding: const EdgeInsets.all(15.0),
         child: Form(
+            key: _form,
             child: SingleChildScrollView(
-          child: Column(children: <Widget>[
-            TextFormField(
-              decoration: InputDecoration(labelText: "First name"),
-              textInputAction: TextInputAction.next,
-              keyboardType: TextInputType.name,
-            ),
-            TextFormField(
-              decoration: InputDecoration(labelText: "Last name"),
-              textInputAction: TextInputAction.next,
-              keyboardType: TextInputType.name,
-            ),
-            TextFormField(
-              decoration: InputDecoration(labelText: "Address"),
-              keyboardType: TextInputType.multiline,
-              maxLines: 3,
-            ),
-            DropdownButtonFormField(
-              decoration: InputDecoration(labelText: "Select an Area"),
-              value: selectedArea,
-              items: Areas.map((String option) {
-                return DropdownMenuItem<String>(
-                  value: option,
-                  child: Text(option),
-                );
-              }).toList(),
-              onChanged: (String? newValue) {
-                setState(() {
-                  selectedArea = newValue;
-                });
-              },
-            ),
-            TextFormField(
-              decoration: InputDecoration(labelText: "Landline"),
-              textInputAction: TextInputAction.next,
-              keyboardType: TextInputType.phone,
-            ),
-            TextFormField(
-              decoration: InputDecoration(labelText: "Mobile"),
-              textInputAction: TextInputAction.next,
-              keyboardType: TextInputType.phone,
-            ),
-            SizedBox(
-              height: 15,
-            ),
-            Row(children: <Widget>[
-              Container(
-                width: 150,
-                height: 100,
-                decoration: BoxDecoration(
-                    border: Border.all(width: 1, color: Colors.grey)),
-                alignment: Alignment.center,
-                child: id_front != null
-                    ? Image.file(
-                        id_front!,
-                        fit: BoxFit.cover,
-                        width: double.infinity,
-                      )
-                    : Text(
-                        "Image of front ID",
-                        textAlign: TextAlign.center,
-                      ),
-              ),
-              SizedBox(
-                width: 15,
-              ),
-              IconButton(
-                  onPressed: takePictureFront,
-                  icon: Icon(Icons.camera_alt_outlined)),
-              //FloatingActionButton(onPressed: () {})
-            ]),
-            SizedBox(
-              height: 15,
-            ),
-            Row(children: <Widget>[
-              Container(
-                width: 150,
-                height: 100,
-                decoration: BoxDecoration(
-                    border: Border.all(width: 1, color: Colors.grey)),
-                alignment: Alignment.center,
-                child: id_back != null
-                    ? Image.file(
-                        id_back!,
-                        fit: BoxFit.cover,
-                        width: double.infinity,
-                      )
-                    : Text(
-                        "Image of Back ID",
-                        textAlign: TextAlign.center,
-                      ),
-              ),
-              SizedBox(
-                width: 15,
-              ),
-              IconButton(
-                  onPressed: takePictureBack,
-                  icon: Icon(Icons.camera_alt_outlined)),
-            ]),
-          ]),
-        )),
+              child: Column(children: <Widget>[
+                TextFormField(
+                  decoration: InputDecoration(labelText: "First name"),
+                  textInputAction: TextInputAction.next,
+                  keyboardType: TextInputType.name,
+                  validator: (value) {
+                    if(value!.isEmpty){
+                      return "First name cannot be empty";
+                    }else if(value.length<3){
+                      return "First name cannot be less than 3 characters";
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    _editedUser.firstName = value;
+                  },
+                ),
+                TextFormField(
+                  decoration: InputDecoration(labelText: "Last name"),
+                  textInputAction: TextInputAction.next,
+                  keyboardType: TextInputType.name,
+                  validator: (value) {
+                    if(value!.isEmpty){
+                      return "Last name cannot be empty";
+                    }else if(value.length<3){
+                      return "Last name cannot be less than 3 characters";
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    _editedUser.lastName = value;
+                  },
+                ),
+                TextFormField(
+                  decoration: InputDecoration(labelText: "Address"),
+                  keyboardType: TextInputType.multiline,
+                  maxLines: 3,
+                  validator: (value) {
+                    if(value!.isEmpty){
+                      return "Address cannot be empty";
+                    }else if(value.length<10){
+                      return "Address is too short";
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    _editedUser.address = value;
+                  },
+                ),
+                DropdownButtonFormField(
+                  decoration: InputDecoration(labelText: "Select an Area"),
+                  value: selectedArea,
+                  items: Areas.map((String option) {
+                    return DropdownMenuItem<String>(
+                      value: option,
+                      child: Text(option),
+                    );
+                  }).toList(),
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      selectedArea = newValue;
+                    });
+                  },
+                  validator:(value) {
+                    if(value==null){
+                      return "Area cannot be empty";
+                    }
+                    if(value.isEmpty){
+                      return "Area cannot be empty";
+                    }
+                    return null;
+                  } ,
+                  onSaved: (value) {
+                    _editedUser.area = value;
+                  },
+                ),
+                TextFormField(
+                  decoration: InputDecoration(labelText: "Landline"),
+                  textInputAction: TextInputAction.next,
+                  keyboardType: TextInputType.phone,
+                  validator: (value) {
+                    if(value!.isEmpty){
+                      return "Landline cannot be empty";
+                    }else if(value.length<10){                      
+                      return "Landline is too short";
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    _editedUser.landline = value;
+                  },
+                ),
+                TextFormField(
+                  decoration: InputDecoration(labelText: "Mobile"),
+                  textInputAction: TextInputAction.done,
+                  keyboardType: TextInputType.phone,
+                  validator: (value) {
+                    if(value!.isEmpty){
+                      return "Mobile number cannot be empty";
+                    }else if(value.length<11){                      
+                      return "Mobile number is too short";
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    _editedUser.mobile = value;
+                  },
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                Row(children: <Widget>[
+                  Container(
+                    width: 150,
+                    height: 100,
+                    decoration: BoxDecoration(
+                        border: Border.all(width: 1, color: Colors.grey)),
+                    alignment: Alignment.center,
+                    child: id_front != null
+                        ? Image.file(
+                            id_front!,
+                            fit: BoxFit.cover,
+                            width: double.infinity,
+                          )
+                        : Text(
+                            "Image of front ID",
+                            textAlign: TextAlign.center,
+                          ),
+                  ),
+                  SizedBox(
+                    width: 15,
+                  ),
+                  IconButton(
+                      onPressed: takePictureFront,
+                      icon: Icon(Icons.camera_alt_outlined)),
+                  //FloatingActionButton(onPressed: () {})
+                ]),
+                SizedBox(
+                  height: 15,
+                ),
+                Row(children: <Widget>[
+                  Container(
+                    width: 150,
+                    height: 100,
+                    decoration: BoxDecoration(
+                        border: Border.all(width: 1, color: Colors.grey)),
+                    alignment: Alignment.center,
+                    child: id_back != null
+                        ? Image.file(
+                            id_back!,
+                            fit: BoxFit.cover,
+                            width: double.infinity,
+                          )
+                        : Text(
+                            "Image of Back ID",
+                            textAlign: TextAlign.center,
+                          ),
+                  ),
+                  SizedBox(
+                    width: 15,
+                  ),
+                  IconButton(
+                      onPressed: takePictureBack,
+                      icon: Icon(Icons.camera_alt_outlined)),
+                ]),
+              ]),
+            )),
       ),
     );
   }
